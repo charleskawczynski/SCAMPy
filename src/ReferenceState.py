ec769bb8dd1d38c48a44179527137ee3b7c3c66e
Charles_Kawczynski <kawczynski.charles@gmail.com>
2019-07-05 23:33:44 -0700

    Removed warning in RefState

--- src/ReferenceState.py
+++ src/ReferenceState.py
@@ -70,13 +70,15 @@ class ReferenceState:
         for k in grid.over_elems_real(Node()):
             temperature[k], ql[k] = eos_entropy(p_[k], self.qtg, self.sg)
             qv[k] = self.qtg - (ql[k] + qi[k])
-            alpha[k] = alpha_c(p_[k], temperature[k], self.qtg, qv[k])
+            self.alpha0[k] = alpha_c(p_[k], temperature[k], self.qtg, qv[k])
+            self.rho0[k] = 1.0/self.alpha0[k]
 
         # for k in grid.over_elems(Center()):
         for k in grid.over_elems_real(Center()):
             temperature_half[k], ql_half[k] = eos_entropy(p_half_[k], self.qtg, self.sg)
             qv_half[k] = self.qtg - (ql_half[k] + qi_half[k])
-            alpha_half[k] = alpha_c(p_half_[k], temperature_half[k], self.qtg, qv_half[k])
+            self.alpha0_half[k] = alpha_c(p_half_[k], temperature_half[k], self.qtg, qv_half[k])
+            self.rho0_half[k] = 1.0/self.alpha0_half[k]
 
         # Now do a sanity check to make sure that the Reference State entropy profile is uniform following
         # saturation adjustment
@@ -86,15 +88,11 @@ class ReferenceState:
                 print('Error in reference profiles entropy not constant !')
                 print('Likely error in saturation adjustment')
 
-        self.alpha0_half[:] = alpha_half[:]
-        self.alpha0[:] = alpha[:]
         self.p0[:] = p_[:]
         self.p0_half[:] = p_half[:]
-        self.rho0[:] = 1.0 / np.array(self.alpha0[:])
-        self.rho0_half[:] = 1.0 / np.array(self.alpha0_half[:])
 
-        self.alpha0_half.extrap(grid)
         self.alpha0.extrap(grid)
+        self.alpha0_half.extrap(grid)
         self.p0.extrap(grid)
         self.p0_half.extrap(grid)
         self.rho0.extrap(grid)
@@ -127,19 +125,19 @@ class ReferenceState:
         k_2 = grid.boundary(Zmax())
 
         Stats.add_reference_profile('alpha0')
-        Stats.write_reference_profile('alpha0', alpha[k_1:k_2])
+        Stats.write_reference_profile('alpha0', self.alpha0[k_1:k_2])
         Stats.add_reference_profile('alpha0_half')
-        Stats.write_reference_profile('alpha0_half', alpha_half[k_1:k_2])
+        Stats.write_reference_profile('alpha0_half', self.alpha0_half[k_1:k_2])
 
         Stats.add_reference_profile('p0')
-        Stats.write_reference_profile('p0', p_[k_1:k_2])
+        Stats.write_reference_profile('p0', self.p0[k_1:k_2])
         Stats.add_reference_profile('p0_half')
-        Stats.write_reference_profile('p0_half', p_half[k_1:k_2])
+        Stats.write_reference_profile('p0_half', self.p0_half[k_1:k_2])
 
         Stats.add_reference_profile('rho0')
-        Stats.write_reference_profile('rho0', 1.0 / np.array(alpha[k_1:k_2]))
+        Stats.write_reference_profile('rho0', self.rho0[k_1:k_2])
         Stats.add_reference_profile('rho0_half')
-        Stats.write_reference_profile('rho0_half', 1.0 / np.array(alpha_half[k_1:k_2]))
+        Stats.write_reference_profile('rho0_half', self.rho0_half[k_1:k_2])
 
         return
 
