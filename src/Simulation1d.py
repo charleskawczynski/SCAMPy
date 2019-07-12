52e8ba328db777feefa1b208fc001e787a36bcb7
Charles Kawczynski <kawczynski.charles@gmail.com>
2019-07-12 08:32:56 -0700

    Added fields to tmp

--- src/Simulation1d.py
+++ src/Simulation1d.py
@@ -72,6 +72,8 @@ class Simulation1d:
                      ('K_m'           , Center() , Neumann(), 1),
                      ('K_h'           , Center() , Neumann(), 1),
                      ('l_mix'         , Center() , Neumann(), 1),
+                     ('q_liq'         , Center() , Neumann(), N_sd),
+                     ('T'             , Center() , Neumann(), N_sd),
                      ('mf_θ_liq'      , Node() , Neumann(), N_sd),
                      ('mf_q_tot'      , Node() , Neumann(), N_sd),
                      ('mf_tend_θ_liq' , Node() , Neumann(), N_sd),
